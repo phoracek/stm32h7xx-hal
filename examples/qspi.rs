ba43b541b7b3405482f4f3593f4a20025ccdf241
Petr Horacek <hrck@protonmail.com>
2021-07-30 21:12:30 +0200

    Test QSPI on DaisySeed
    It does not seem to work though.
    
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- examples/qspi.rs
+++ examples/qspi.rs
@@ -26,17 +26,20 @@ fn main() -> ! {
     // Acquire the GPIO peripherals. This also enables the clock for
     // the GPIOs in the RCC register.
     let gpiog = dp.GPIOG.split(ccdr.peripheral.GPIOG);
-    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
-    let gpiod = dp.GPIOD.split(ccdr.peripheral.GPIOD);
-    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+    let _gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    let _gpiod = dp.GPIOD.split(ccdr.peripheral.GPIOD);
+    let _gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+    let gpiof = dp.GPIOF.split(ccdr.peripheral.GPIOF);
 
     let _qspi_cs = gpiog.pg6.into_alternate_af10();
 
-    let sck = gpiob.pb2.into_alternate_af9();
-    let io0 = gpiod.pd11.into_alternate_af9();
-    let io1 = gpiod.pd12.into_alternate_af9();
-    let io2 = gpioe.pe2.into_alternate_af9();
-    let io3 = gpiod.pd13.into_alternate_af9();
+    // Pins taken from:
+    // https://github.com/electro-smith/libDaisy/blob/3dda55e9ed55a2f8b6bc4fa6aa2c7ae134c317ab/src/per/qspi.c#L704
+    let sck = gpiof.pf10.into_alternate_af9();
+    let io0 = gpiof.pf8.into_alternate_af10();
+    let io1 = gpiof.pf9.into_alternate_af10();
+    let io2 = gpiof.pf7.into_alternate_af9();
+    let io3 = gpiof.pf6.into_alternate_af9();
 
     info!("");
     info!("stm32h7xx-hal example - QSPI");
@@ -52,10 +55,14 @@ fn main() -> ! {
 
     qspi.configure_mode(QspiMode::FourBit).unwrap();
 
-    loop {
-        qspi.write(0x00, &[0xAA, 0x00, 0xFF]).unwrap();
+    let test = [0xAA, 0x00, 0xFF];
 
-        let mut read: [u8; 3] = [0; 3];
-        qspi.read(0xFF, &mut read).unwrap();
-    }
+    qspi.write(0x00, &test).unwrap();
+
+    let mut read: [u8; 3] = [0; 3];
+    qspi.read(0x00, &mut read).unwrap();
+
+    assert_eq!(read, test);
+
+    loop {}
 }
